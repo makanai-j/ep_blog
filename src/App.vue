b780d035f9017db622f2986df6f811a5c608fbfe
makanai <nonoise.zero@gmail.com>
2023-09-06 09:45:41 +0900

    20230906

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,32 @@
 <script setup>
+import { ref } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
+
+let title = 'ep'
+const content = ref('')
+const count = ref(0)
+
+let plus = (x) => {
+  count.value += x
+}
+
+
+
+let changeFn = () => {
+  if (fn.value == 'makanai') {
+    fn.value = 'jumpei'
+  } else {
+    fn.value = 'makanai'
+  }
+}
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld :title="title" :content="content" @plus="plus" @change-fn="changeFn" :count="count">
+    <template #firstName="{ text }">
+      {{ text }}
+    </template>
+  </HelloWorld>
+  <input v-model="content" />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,39 +2,24 @@
 import { ref } from 'vue'
 
 defineProps({
-  msg: String,
+  title: String,
+  content: String,
+  count: Number,
 })
+const emit = defineEmits(['plus', 'changeFn'])
 
-const count = ref(0)
+let plus_ = () => {
+  emit('plus', num.value)
+  emit('changeFn')
+}
+
+const num = ref(4)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <h1>{{ title }}</h1>
+  <p>{{ content }}</p>
+  <button @click="plus_">plus {{ num }}</button>
+  <h3>{{ count }}</h3>
+  <slot name="firstName" :text="fn"></slot>
 </template>
-
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
