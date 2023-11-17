75eeb9b45a98dfddafd7b523a5df1758295ccfe3
makanai_sub <nonoise.zero@gmail.com>
2023-11-17 16:30:58 +0900

    torima

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,39 @@
 <script setup>
 import Header from './components/ui_parts/MainHeader.vue'
 import Footer from './components/ui_parts/MainFooter.vue'
+
+onMounted(() => {
+  let scrollBarWidth = window.innerWidth - document.documentElement.clientWidth
+  document.documentElement.style.setProperty('--scrollBarWidth', `${scrollBarWidth}px`)
+})
 </script>
 
 <template>
   <Header></Header>
   <div class="main">
-    <router-view />
+    <div class="section-content">
+      <router-view />
+    </div>
   </div>
   <Footer></Footer>
 </template>
 
 <style>
+:root {
+  --scrollBarWidth: 0px;
+}
 @font-face {
   font-family: 'customFont';
   src: url('../NotoSerifJP-Regular_.woff');
-  /*src: url('../_other/NotoSerifJP-Bold.otf');*/
-  /*src: url('../_other/NotoSerifJP-Bold.otf');*/
 }
 .main {
-  width: 100vw;
+  width: calc(100vw - var(--scrollBarWidth));
+  background: black;
+}
+.section-content {
+  width: 95%;
+  margin: auto;
+  background: white;
 }
 .router-link-active {
   font-weight: bold;
