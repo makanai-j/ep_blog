b1dfeebdbb487e06c2a02d326b0364dd8e57e3ce
makanai-j <kd1365575@st.kobedenshi.ac.jp>
2023-11-18 18:44:10 +0900

    カルーセル追加完了　次：タブプレビューとトピック追加

--- src/components/pages/DiaryListPage.vue
+++ src/components/pages/DiaryListPage.vue
@@ -1,8 +1,10 @@
 <script setup>
 import { useRouter } from 'vue-router'
 import DiaryList from '../ui_parts/DiaryList.vue'
+import ImageSlider from '../ui_parts/ImageSlider.vue'
 </script>
 
 <template>
   <DiaryList></DiaryList>
+  <ImageSlider> click </ImageSlider>
 </template>
